cba8b3eba791da15ccbf301b4d91723d41881681
astrakho <30877992+astrakho@users.noreply.github.com>
2019-11-18 23:24:42 -0800

    Immutably Updates the board with Start Locations
    A Board is Clickable and a Start SVG is Rendered on the clicked Square. The Whole Board rerenderes on a Square Click.

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,44 +1,81 @@
-import React, { Component } from 'react';
-import Square from '../Square/Square';
-import './Board.css'
+import React, { Component } from "react";
+import Square from "../Square/Square";
+import "./Board.css";
+import { throwStatement } from "@babel/types";
 
 class Board extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       grid: []
     };
   }
-  
+
   componentDidMount() {
     const grid = initializeGrid();
-    this.setState({grid});
+    this.setState({ grid });
   }
 
-  render(){
-    return(
-      <div className='Board'>
-        {this.state.grid}
+  handleMouseClick(row, col) {
+    const newGrid = updateGridImmutably(this.state.grid, row, col);
+    this.setState({ grid: newGrid });
+  }
+
+  render() {
+    let gridToShow = this.state.grid.map((row, r) => {
+      return row.map((col, c) => {
+        return (
+          <Square
+            key={`${r}-${c}`}
+            row={r}
+            col={c}
+            isStart={this.state.grid[r][c].isStart}
+            onClick={(r, c) => this.handleMouseClick(r, c)}
+          />
+        );
+      });
+    });
+
+    return (
+      <div className="Board">
+        {gridToShow.length > 0 ? gridToShow : "Sorry, Nothing to Display"}
       </div>
-    )
+    );
   }
 }
 
+// Function to Initialize Grid
 const initializeGrid = () => {
   const grid = [];
-  for (let row = 0; row < 15; row++) {
+  for (let row = 0; row < 25; row++) {
     const currentRow = [];
     for (let col = 0; col < 50; col++) {
       currentRow.push(createSquare(row, col));
     }
     grid.push(currentRow);
   }
-  console.log(grid);
   return grid;
 };
 
+// Creates an Objrct, representing a Square's State
 const createSquare = (r, c) => {
-  return <Square row={r} col={c} isStart = {false}/>
-}
+  return {
+    row: r,
+    col: c,
+    isStart: false
+  };
+};
+
+// Updates Grid Immutably, by creating a new grid reference
+const updateGridImmutably = (grid, r, c) => {
+  const newGrid = grid.slice(); // Return a shallow copy of an array
+  const square = newGrid[r][c];
+  const newSquare = {
+    ...square,
+    isStart: !square.isStart
+  };
+  newGrid[r][c] = newSquare;
+  return newGrid;
+};
 
 export default Board;
--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import './Square.css';
-import Start from '../../icons/start'
+import React, { Component } from "react";
+import "./Square.css";
+import Start from "../../icons/start";
 
-class Square extends Component{
+class Square extends Component {
   render() {
-    const {
-      row,
-      col,
-      isStart,
-      isTarget
-    } = this.props;
+    const { row, col, isStart, isTarget, onClick } = this.props;
 
     return (
-      <div className='Square' id = {`node-${row}-${col}`}>
+      <div
+        className="Square"
+        id={`node-${row}-${col}`}
+        onClick={() => onClick(row, col)}
+      >
         {isStart === true ? <Start /> : null}
       </div>
     );
