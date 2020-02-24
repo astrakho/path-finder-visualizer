dc14b999263d5a971d11655185d96e03c98df378
astrakho <30877992+astrakho@users.noreply.github.com>
2020-02-23 21:40:49 -0800

    Dijkstra Search, using shallow copy array

--- src/algorithms/dijkstra.js
+++ src/algorithms/dijkstra.js
@@ -19,65 +19,59 @@ export function dijkstra(grid, source) {
   unVisitedNodes = flattenGraph(grid, source);
 
   // Set Starting Node distance to 0
-  unVisitedNodes[(sourceX*50) + sourceY].distance = 0;
+  unVisitedNodes[sourceX * 50 + sourceY].distance = 0;
 
-  while (unVisitedNodes.length > 0){
-
-    // Get Node with Shortest distance
+  while (unVisitedNodes.length > 0) {
+    // Sort Nodes with Shortest distance coming
     sortNodesByDistance(unVisitedNodes);
-    
+
     // Remove Picked Node from Unvisited Array, essentially marking a Node visited
     let minNode = unVisitedNodes.shift();
-
-    console.log(minNode);
 
     let neighbors = getNeighbors(minNode, grid);
 
     // For each neighboring Node, update distances if needed
-    for(const v of neighbors){
+    for (const v of neighbors) {
+      if (v.isWall === true) {
+        continue;
+      }
       v.distance = minNode.distance + 1;
-      if(v.isVisited == null){
+      v.prev = minNode;
+      if (v.isVisited == null) {
         visitedNodesInOrder.push(v);
         v.isVisited = true;
       }
     }
-    
   }
 
-  console.log(visitedNodesInOrder);
-
   // Return an array of distances
-  return(visitedNodesInOrder);
-
+  return visitedNodesInOrder;
 }
 
 // Flattens the Graph and assigns Distance of Infinity to each square
 // Needs refactoring to use declarative style as opposed to a double for loop
-function flattenGraph(grid){
-  const squares = [];
-  for (const row of grid){
-    for (const square of row){
-      const newSquare = {
-        ...square,
-        distance: Infinity
-      }
-      squares.push(newSquare);
+function flattenGraph(grid) {
+  const nodes = [];
+  for (const row of grid) {
+    for (const node of row) {
+      node.distance = Infinity;
+      nodes.push(node);
     }
   }
-  return squares;
+  return nodes;
 }
 
 function getNeighbors(node, grid) {
-  const neighbors = [];
-  const {row, col} = node;
+  let neighbors = [];
+  const { row, col } = node;
   if (row > 0) neighbors.push(grid[row - 1][col]);
   if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
   if (col > 0) neighbors.push(grid[row][col - 1]);
-  if (col < grid.length - 1) neighbors.push(grid[row][col + 1]);
+  if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
   return neighbors;
 }
 
 // Sort Nodes By Distance
-function sortNodesByDistance(dNodes){
+function sortNodesByDistance(dNodes) {
   dNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
 }
